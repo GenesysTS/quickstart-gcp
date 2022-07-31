a7cb863abd4d8803bca688c6f9d00f047256d8aa
Dmitry Mizin <dmitry.mizin@genesys.com>
2022-07-31 15:44:57 -0700

    provide issue

--- platform/tfm/10-misc/main.tf
+++ platform/tfm/10-misc/main.tf
@@ -16,10 +16,12 @@ resource "consul_config_entry" "service_intentions" {
 
 ### Create MSSQL gvp-rs DB and users/logins
 resource "mssql_database" "gvp-rs" {
+  provider = mssql
   name      = "gvp_rs"
 }
 
 resource "mssql_sql_login" "gkeadmin" {
+  provider = mssql
   name                      = "gkeadmin"
   password                  = "Genesys@123"
   must_change_password      = false
@@ -29,6 +31,7 @@ resource "mssql_sql_login" "gkeadmin" {
 }
 
 resource "mssql_sql_login" "mssqlreader" {
+  provider = mssql
   name                      = "mssqlreader"
   password                  = "Genesys@123"
   must_change_password      = false
@@ -38,6 +41,7 @@ resource "mssql_sql_login" "mssqlreader" {
 }
 
 resource "mssql_sql_user" "gkeadmin" {
+  provider = mssql
   name        = "example"
   database_id = data.mssql_database.gvp-rs.id
   login_id    = mssql_sql_login.gkeadmin.id
@@ -45,6 +49,7 @@ resource "mssql_sql_user" "gkeadmin" {
 }
 
 resource "mssql_sql_user" "mssqlreader" {
+  provider = mssql
   name        = "example"
   database_id = data.mssql_database.gvp-rs.id
   login_id    = mssql_sql_login.mssqlreader.id
@@ -52,6 +57,7 @@ resource "mssql_sql_user" "mssqlreader" {
 }
 
 resource "mssql_database_role" "gkeadmin" {
+  provider = mssql
   name        = "db_owner"
   database_id = data.mssql_database.gkeadmin.id
   owner_id    = data.mssql_sql_user.gkeadmin.id
@@ -59,6 +65,7 @@ resource "mssql_database_role" "gkeadmin" {
 }
 
 resource "mssql_database_role" "mssqlreader" {
+  provider = mssql
   name        = "db_datareader"
   database_id = data.mssql_database.gkeadmin.id
   owner_id    = data.mssql_sql_user.mssqlreader.id
